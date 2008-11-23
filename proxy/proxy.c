bd9956ccbcd8a98c6ad816712170c27a8f10ac75
Adam Borowski <kilobyte@angband.pl>
2008-11-23 01:34:20 +0100

    FreeBSD portability

--- proxy/proxy.c
+++ proxy/proxy.c
@@ -15,6 +15,9 @@
 #ifdef HAVE_SYS_SOCKET_H
 # include <sys/socket.h>
 #endif
+#ifdef HAVE_NETINET_IN_H
+# include <netinet/in.h>
+#endif
 #include <errno.h>
 #include <assert.h>
 #include <getopt.h>
