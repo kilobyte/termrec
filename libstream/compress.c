46c6054f3a9488d775d8825368f06b55c1ff52d2
Adam Borowski <kilobyte@angband.pl>
2017-11-02 07:52:39 +0100

    Make a macro function-like.

--- libstream/compress.c
+++ libstream/compress.c
@@ -34,7 +34,7 @@ VISIBILITY_DISABLE
 #include "error.h"
 #include "gettext.h"
 
-#define ERRORMSG(x) {if (write(fd,(x),strlen(x))) {};}
+#define ERRORMSG(x) do if (write(fd,(x),strlen(x))) {} while(0)
 
 #define BUFFER_SIZE 32768
 
