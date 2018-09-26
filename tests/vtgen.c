eadd41abb948f472ed3ef79f49497b5de02acf39
Adam Borowski <kilobyte@angband.pl>
2018-09-26 18:42:24 +0200

    tests: add more ANSI codes to the fuzzer

--- tests/vtgen.c
+++ tests/vtgen.c
@@ -14,6 +14,15 @@ static char rndchar(const char *s)
     return s[rnd(strlen(s))];
 }
 
+#define ARRAYSZ(x) (sizeof(x)/sizeof(x[0]))
+
+static const char *misc[]=
+{
+    "\n", "\r", "\b", "\x0c", "\t",
+    "\e[?7h", "\e[?7l",
+    "\e(0", "\e)0", "\e(B", "\e)B", "\x0e", "\x0f",
+};
+
 int main()
 {
     while (1)
@@ -30,10 +39,13 @@ int main()
             printf("\xcc\x80");
             break;
         case 3:
-            switch (rnd(8))
+            switch (rnd(12))
             {
             case 0:
                 printf("\e%c", rndchar("78DEM"));
+                break;
+            case 1: case 2: case 3:
+                puts(misc[rnd(ARRAYSZ(misc))]);
                 break;
             default:
                 printf("\e[%d;%d%c", rnd(5), rnd(5), rndchar("mDCFAEBrJKLM@PXfGdc"));
