e75f43be61781a3f4c627e70901240d7be9fc893
Adam Borowski <kilobyte@angband.pl>
2022-09-03 01:44:03 +0200

    Update autotoolage via autoupdate.

--- libtty/formats.h
+++ libtty/formats.h
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#if TIME_WITH_SYS_TIME
+#include <time.h>
+#if HAVE_SYS_TIME_H
 # include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
 #endif
 
 typedef void play_func(FILE *f,
